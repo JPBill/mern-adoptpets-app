6a718aed13e9f80aecb7f32883c82f97a8d18421
Juan Pablo Bill <juampibill1995@gmail.com>
2024-02-14 13:26:41 -0300

    complete register page funcionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,59 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline';
 
 const Register = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
 
   const handlePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/server/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/iniciar-sesion');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <main className="flex-1 py-8 lg:pl-64">
       <div className="px-4 sm:px-6 lg:px-8 lg:max-w-6xl lg:mx-auto">
-        <form className="space-y-8 divide-y divide-gray-200">
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-8 divide-y divide-gray-200"
+        >
           <div>
             <div>
               <h3 className="text-lg leading-6 font-medium text-gray-900 lg:text-xl">
@@ -43,6 +84,7 @@ const Register = () => {
                     name="username"
                     id="username"
                     required
+                    onChange={handleChange}
                     autoComplete="name"
                     className="block py-2 px-3 w-full bg-white rounded-md text-sm border border-gray-300 focus:outline-cyan-600"
                   />
@@ -62,6 +104,7 @@ const Register = () => {
                     name="email"
                     id="email"
                     required
+                    onChange={handleChange}
                     autoComplete="email"
                     className="block py-2 px-3 w-full bg-white rounded-md text-sm border border-gray-300 focus:outline-cyan-600"
                   />
@@ -81,6 +124,7 @@ const Register = () => {
                     id="password"
                     name="password"
                     required
+                    onChange={handleChange}
                     className="block py-2 px-3 w-full bg-white rounded-md text-sm border border-gray-300 focus:outline-cyan-600"
                   />
                   <div
@@ -103,12 +147,16 @@ const Register = () => {
               <button className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm md:text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
                 Continuar con Google
               </button>
-              <button className="mt-3 ml-0 md:mt-0 md:ml-3 justify-center py-2 px-4 border border-transparent shadow-sm text-sm md:text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none">
-                Registrarme
+              <button
+                disabled={loading}
+                className="mt-3 ml-0 md:mt-0 md:ml-3 justify-center py-2 px-4 border border-transparent shadow-sm text-sm md:text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none"
+              >
+                {loading ? 'Cargando...' : 'Registrarme'}
               </button>
             </div>
           </div>
         </form>
+        {error && <p className="mt-6 text-sm text-red-800">{error}</p>}
       </div>
     </main>
   );
