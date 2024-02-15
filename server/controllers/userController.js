b2a73da48afe4d1d8cecfab840af99061c7281cc
Juan Pablo Bill <juampibill1995@gmail.com>
2024-02-15 11:18:57 -0300

    create get user pet listing api route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/userModel.js';
+import Listing from '../models/listingModel.js';
 
 export const test = (req, res) => {
   res.json({
@@ -44,5 +45,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json('Usuario eliminado.');
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'Solamente puedes ver tu listado.'));
   }
 };
